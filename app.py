3f93d26d092ac54fc67987c103db2b188d284167
Jeff-Bento <mkt@keyseven.com.br>
2022-08-11 20:08:14 -0300

    teste1

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
-
+from home.home import home_bp
 
 
 app = Flask(__name__)
 
+app.register_blueprint(home_bp)
 
-@app.route('/')
-def index():
-    return "Pagina em Flask"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- home/home.py
+++ home/home.py
@@ -2,5 +2,8 @@ from flask import Blueprint
 from flask import Flask, render_template, session, redirect
 
 
-home_bp = Blueprint('home',__name__, template_folder='./templates',static_folder='./static', static_url_path='./static')
+home_bp = Blueprint('home',__name__, template_folder='./templates',static_folder='./static')
 
+@home_bp.route('/')
+def index():
+    return "Pagina Python"
